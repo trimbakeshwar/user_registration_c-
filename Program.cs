5f251410f2934918151fbbbfcfb04322390ae19a
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-04-02 11:40:52 +0530

    Added:-validate first name

--- Program.cs
+++ Program.cs
@@ -1,9 +1,27 @@
 ﻿using System;
-    class Program
+using System.Text.RegularExpressions;
+
+public class userRegistration
+{
+    public void validFirstName(String name)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("user registration ");
-           
-        }
+        var expression = "^[A-Z][a-z]{3,}$";
+        var match = Regex.Match(name, expression, RegexOptions.None);
+        if (match.Success)
+            Console.WriteLine("valid");
+        else
+            Console.WriteLine("invalid");
     }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("ENTER NAME : ");
+        String name = Console.ReadLine();
+        userRegistration user = new userRegistration();
+        user.validFirstName(name);
+
+    }
+}
